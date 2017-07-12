32cbe2d21ab2f6ac0562a01ca21c7b5a0d5d284a
scarymrgrey <imbamrgrey@gmail.com>
2017-07-12 22:50:55 +0300

    added db adjust at startup

--- DemoPayments/Startup.cs
+++ DemoPayments/Startup.cs
@@ -2,18 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Incoding.Data;
+using Microsoft.IdentityModel.Protocols;
+using NHibernate.Context;
+using NHibernate.Tool.hbm2ddl;
+using Operations.Persistance;
 
 namespace DemoPayments
 {
     public class Startup
     {
+        void configureDataBase()
+        {
+            var configure = Fluently
+                .Configure()
+                .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(Configuration.GetConnectionString("BloggingDatabase")))
+                .CurrentSessionContext<CallSessionContext>()
+                .Mappings(configuration => configuration.FluentMappings
+                    .AddFromAssembly(typeof(User).Assembly));
+            var dbManager = new NhibernateManagerDataBase(configure);
+            dbManager
+
+        }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            configureDataBase();
         }
 
         public IConfiguration Configuration { get; }
---
+++ Opertations/Persistance/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Incoding.Data;
+
+namespace Operations.Persistance
+{
+   public class User : EntityBase
+    {
+        public virtual string Name { get; set; }
+
+        public class Map : NHibernateEntityMap<User>
+        {
+            protected Map()
+            {
+                Id(r => r.Id).GeneratedBy.Assigned();
+                MapEscaping(r => r.Name);
+            }
+        }
+    }
+}
